0e52e924c2ac8e70885043d5434a04a0a85d771d
Joseph Flatley <josephflatley@Josephs-MacBook-Pro.local>
2023-06-09 16:19:05 -0500

    less rough

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
-const commentRoutes = require('./comment-routes.js');
+const commentRoutes = require('./commentRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
