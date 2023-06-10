b19316f66af70aa5b8e3d85e106bfed28eff9e88
Joseph Flatley <josephflatley@Josephs-MBP.lan1>
2023-06-10 13:17:56 -0500

    cleaned up

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,47 +1,25 @@
-const newFormHandler = async (event) => {
+const commentFormHandler = async function (event) {
 	event.preventDefault();
 
-	const title = document.querySelector('#blog-title').value.trim();
+	const blog_id = document.querySelector('.new-comment-form').dataset.blogid;
 
-	const description = document.querySelector('#blog-desc').value.trim();
+	const comment_description = document.querySelector('#comment_description').value.trim();
 
-	if (title && description) {
-		const response = await fetch(`/api/blogs`, {
+	if (comment_description) {
+		await fetch('/api/comments', {
 			method: 'POST',
-			body: JSON.stringify({ title, description }),
+			body: JSON.stringify({
+				blog_id,
+				comment_description,
+			}),
 			headers: {
-				'Content-Type': 'application/json',
-			},
+				'Content-Type': 'application/json'
+			}
 		});
-
-		if (response.ok) {
-			document.location.replace('/dashboard');
-		} else {
-			alert('Failed to create blog');
-		}
-	}
-};
-
-const delButtonHandler = async (event) => {
-	if (event.target.hasAttribute('data-id')) {
-		const id = event.target.getAttribute('data-id');
-
-		const response = await fetch(`/api/blogs/${id}`, {
-			method: 'DELETE',
-		});
-
-		if (response.ok) {
-			document.location.replace('/dashboard');
-		} else {
-			alert('Failed to delete blog');
-		}
+		document.location.reload();
 	}
 };
 
 document
-	.querySelector('.new-blog-form')
-	.addEventListener('submit', newFormHandler);
-
-document
-	.querySelector('.blog-list')
-	.addEventListener('click', delButtonHandler);
+	.querySelector('.new-comment-form')
+	.addEventListener('submit', commentFormHandler);
